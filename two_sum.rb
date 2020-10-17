cd6a952f6f80a903b0572eeb934384c81653e07a
kevin park <kevin.park02@gmail.com>
2020-10-17 11:49:08 -0700

    Finish two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def bad_two_sum?(arr, target)
     perfect_sum = []
 
@@ -15,9 +17,36 @@ arr = [0, 1, 5, 7]
 p bad_two_sum?(arr, 6) # => should be true
 p bad_two_sum?(arr, 10) # => should be false
 
-def okay_two_sum?(arr, target)
-   sorted = arr.sort 
-   mid = arr.length/2
-   return true if arr[mid] == target
-   left = sorted[0]
+# def okay_two_sum?(arr, target)
+#     debugger
+#     sorted = arr.sort 
+#     mid = arr.length/2
+#     return true if arr[mid] == target
+
+#     if target < sorted[mid]
+#         left = sorted[0...mid]
+#         okay_two_sum?(left, target)
+#     else
+#         right = sorted[mid+1..sorted.length-1]
+#         okay_two_sum?(right, target)
+#     end
+#     false
+# end
+
+# arr = [0, 1, 5, 7]
+# p okay_two_sum?(arr, 6) # => should be true
+# p okay_two_sum?(arr, 10) # => should be false
+
+def two_sum?(arr, target)
+    hash = Hash.new
+
+    arr.each do |ele|
+        return true if hash[target - ele]
+        hash[ele] = true
+    end
+    false
 end
+
+arr = [0, 1, 5, 7]
+p two_sum?(arr, 6)
+p two_sum?(arr, 10)
